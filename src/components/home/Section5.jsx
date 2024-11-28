e3b18f6e5b096b2f934fac9acc5d01bed5679fd5
Ayush Ahirwar <ayushahirwar6673@gmail.com>
2024-11-28 17:15:03 +0530

    clientele

--- src/components/home/Section5.jsx
+++ src/components/home/Section5.jsx
@@ -13,50 +13,51 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 export default function App() {
   return (
-    <div className='w-full h-[30vh] flex items-center justify-center px-[2.5vw]'>
-     <div className='w-full h-full'>
-     <Swiper
-       slidesPerView={4}
-        spaceBetween={2}
-        loop={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
+    <div className='w-full flex items-center flex-col justify-center px-[2.5vw]'>
+      <h2 className='text-[3vw] font-semibold mb-[1vw] text-center'>Our Clientele</h2>
+      <div className='w-full h-[30vh]'>
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={2}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
             <div className='w-full h-full flex items-center justify-center'>
-                <img className='w-[50%] h-[60%] object-contain' src="https://www.allastir.com/images/9wanbury.jpg" alt="" />
+              <img className='w-[50%] h-[60%] object-contain' src="https://www.allastir.com/images/9wanbury.jpg" alt="" />
             </div>
-        </SwiperSlide>
-        <SwiperSlide>
+          </SwiperSlide>
+          <SwiperSlide>
             <div className='w-full h-full flex items-center justify-center'>
-                <img className='w-[50%] h-[60%] object-contain' src="https://www.allastir.com/images/11aris.jpg" alt="" />
+              <img className='w-[50%] h-[60%] object-contain' src="https://www.allastir.com/images/11aris.jpg" alt="" />
             </div>
-        </SwiperSlide>
-        <SwiperSlide>
+          </SwiperSlide>
+          <SwiperSlide>
             <div className='w-full h-full flex items-center justify-center'>
-                <img className='w-[50%] h-[60%] object-contain' src="https://www.allastir.com/images/12akums.jpg" alt="" />
+              <img className='w-[50%] h-[60%] object-contain' src="https://www.allastir.com/images/12akums.jpg" alt="" />
             </div>
-        </SwiperSlide>
-        <SwiperSlide>
+          </SwiperSlide>
+          <SwiperSlide>
             <div className='w-full h-full flex items-center justify-center'>
-                <img className='w-[50%] h-[60%] object-contain' src="https://www.allastir.com/images/13Cipla1.jpg" alt="" />
+              <img className='w-[50%] h-[60%] object-contain' src="https://www.allastir.com/images/13Cipla1.jpg" alt="" />
             </div>
-        </SwiperSlide>
-        <SwiperSlide>
+          </SwiperSlide>
+          <SwiperSlide>
             <div className='w-full h-full flex items-center justify-center'>
-                <img className='w-[50%] h-[60%] object-contain' src="https://www.allastir.com/images/18Unison1.jpg" alt="" />
+              <img className='w-[50%] h-[60%] object-contain' src="https://www.allastir.com/images/18Unison1.jpg" alt="" />
             </div>
-        </SwiperSlide>
-      </Swiper>
-     </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
