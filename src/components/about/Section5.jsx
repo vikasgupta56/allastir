697403598fdc72d73f4c76321eb0451b945f03ae
Ayush Ahirwar <ayushahirwar6673@gmail.com>
2025-02-25 17:28:19 +0530

    update

--- src/components/about/Section5.jsx
+++ src/components/about/Section5.jsx
@@ -6,7 +6,7 @@ const Section5 = () => {
     <div className='px-[2.5vw] sm:px-[4vw] flex sm:flex-col-reverse justify-between w-full min-h-screen py-[8vw] gap-[.5vw]'>
        <div className='w-full h-[100vh] sm:h-fit'>
         <div className="w-full sm:h-[40%] h-[40%]  flex flex-col justify-between pr-[5vw] sm:pb-[5vw] pb-[1.2vw]">
-          <h2 className='sm:text-[10vw] text-[3vw] leading-tight'>We are a Multi-Faceted Design Practice.</h2>
+          <h2 className='sm:text-[10vw] text-[3vw] leading-tight'>Precision and Innovation Across Two Advanced Units.</h2>
           <div>
           <p><strong>Unit 1</strong></p>
           <h4 className='sm:text-[4.5vw] text-[1vw] sm:mt-[2.5vw]'>Designed for precision and efficiency, Unit 1 is our foundation for high-quality API manufacturing. Equipped with advanced technology and rigorous quality control, it drives our commitment to excellence in pharmaceutical production.</h4>
--- src/components/home/Section4.jsx
+++ src/components/home/Section4.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Section4 = () => {
@@ -6,7 +7,7 @@ const Section4 = () => {
             <h2 className='sm:text-[10vw] text-[3vw] font-semibold sm:mb-[3vw] sm:mt-[2vw] mb-[2vw] text-center'>Our Products</h2>
             <div className='w-full sm:h-[60vw]  h-[40vh]  flex items-center overflow-hidden'>
                 <div className='product-marquee w-fit h-full flex items-start sm:gap-[1vw] gap-[.5vw] px-[.25vw]'>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    <Link href='/product' className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[45%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/8eca099bc37b40791bcaefb770c1e95a07dd2ebe-1416x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -16,8 +17,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[85%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/fc18a235ae5d6d22d752aeb9df4af190647246b0-700x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -27,8 +28,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[70%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/80007803615590b3d31a96268109518e0e3a538e-700x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -38,8 +39,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[85%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/6df2923a5a613283f3005f3326c560fef6310784-1416x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -49,8 +50,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[45%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/c17ab0360b99fc853dbacffe76ca234cedb5412b-700x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -60,8 +61,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[45%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/0227727b9c95b12841bee0e48a47430151bca781-1416x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -71,8 +72,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[45%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/8eca099bc37b40791bcaefb770c1e95a07dd2ebe-1416x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -82,8 +83,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[85%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/fc18a235ae5d6d22d752aeb9df4af190647246b0-700x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -93,8 +94,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[70%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/80007803615590b3d31a96268109518e0e3a538e-700x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -104,8 +105,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[85%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/6df2923a5a613283f3005f3326c560fef6310784-1416x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -115,8 +116,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[45%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/c17ab0360b99fc853dbacffe76ca234cedb5412b-700x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -126,8 +127,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[45%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/0227727b9c95b12841bee0e48a47430151bca781-1416x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -137,10 +138,10 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
+                    </Link>
                 </div>
                 <div className='product-marquee w-fit h-full flex items-start sm:gap-[1vw] gap-[.5vw] px-[.25vw]'>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[45%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/8eca099bc37b40791bcaefb770c1e95a07dd2ebe-1416x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -150,8 +151,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[85%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/fc18a235ae5d6d22d752aeb9df4af190647246b0-700x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -161,8 +162,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[70%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/80007803615590b3d31a96268109518e0e3a538e-700x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -172,8 +173,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[85%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/6df2923a5a613283f3005f3326c560fef6310784-1416x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -183,8 +184,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[45%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/c17ab0360b99fc853dbacffe76ca234cedb5412b-700x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -194,8 +195,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[45%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/0227727b9c95b12841bee0e48a47430151bca781-1416x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -205,8 +206,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[45%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/8eca099bc37b40791bcaefb770c1e95a07dd2ebe-1416x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -216,8 +217,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[85%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/fc18a235ae5d6d22d752aeb9df4af190647246b0-700x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -227,8 +228,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[70%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/80007803615590b3d31a96268109518e0e3a538e-700x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -238,8 +239,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[85%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/6df2923a5a613283f3005f3326c560fef6310784-1416x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -249,8 +250,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[45%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/c17ab0360b99fc853dbacffe76ca234cedb5412b-700x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -260,8 +261,8 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
-                    <div className='mq-card sm:w-[40vw] w-[14vw] h-full'>
+                    </Link>
+                    <Link href="/product" className='mq-card sm:w-[40vw] w-[14vw] h-full'>
                         <div className='w-full h-[45%] rounded overflow-hidden'>
                             <img className='w-full h-full object-cover' src="https://cdn.sanity.io/images/givbmsph/production/0227727b9c95b12841bee0e48a47430151bca781-1416x796.jpg?auto=format&w=768" alt="" />
                         </div>
@@ -271,7 +272,7 @@ const Section4 = () => {
                                 <h3 className='sm:text-[4vw] text-[.9vw] capitalize font-semibold whitespace-nowrap'>product name</h3>
                             </span>
                         </div>
-                    </div>
+                    </Link>
                 </div>
             </div>
         </div>
